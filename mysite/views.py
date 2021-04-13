59915912659aad857e5f64791ec059580fca7087
CJCU-CC <108b32079@mailst.cjcu.edu.tw>
2021-04-13 11:43:43 +0800

    0413

--- mysite/views.py
+++ mysite/views.py
@@ -61,5 +61,17 @@ def posting(request):
     return render(request, 'posting.html', locals())
 
 def contact(request):
-    form = forms.ContactForm()
+    if request.method == 'POST':
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            message = "感謝您的來信"
+            user_name = form.cleaned_data['user_name']
+            user_city = form.cleaned_data['user_city']
+            user_school = form.cleaned_data['user_school']
+            user_email = form.cleaned_data['user_email']
+            user_message = form.cleaned_data['user_message']
+        else:
+            message = "請檢查您輸入的資訊是否正確"
+    else:
+        form = forms.ContactForm()
     return render(request, 'contact.html', locals())
